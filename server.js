cf57e7495b869f69e85dcb15e2c35aa4b8b6bfec
Vikrant <vikrantp758@gmail.com>
2021-04-30 17:08:15 +0530

    Cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,15 +10,20 @@ const connectDB = require('./config/db');
 
 connectDB();
 
+//Cors
+const corsOptions ={
+    origin: process.env.ALLOWED_CLIENTS.split(',') ['http://localhost:3000',]
+}
+app.use(cors(corsOptions));
 //Template Engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
 //Routes
 app.use('/api/files', require('./routes/files'));
-
 app.use('/files', require('./routes/show'));
-
 app.use('/files/download', require('./routes/download'));
+
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port ${PORT}`);
